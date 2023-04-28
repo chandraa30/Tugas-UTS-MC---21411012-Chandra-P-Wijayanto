eceff1d701984228ec2e5e21acd13dff2aaea765
chandraa30 <chandra30.pw@gmail.com>
2023-04-28 18:33:04 +0900

    UTS MC - CHANDRA

---
+++ app/src/main/java/com/example/tugaslogreg/isi.java
@@ -0,0 +1,14 @@
+package com.example.tugaslogreg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class isi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_isi);
+    }
+}
